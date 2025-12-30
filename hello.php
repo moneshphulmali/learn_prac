1b6c3933bc040286b251444ff0dc39a51d1ad257
Monesh Phulmali <moneshphulmali9575@gmail.com>
2025-12-30 13:50:13 +0530

    change the color of h1

--- hello.php
+++ hello.php
@@ -1,13 +1,19 @@
-<html>  
+    <html>  
 
-    <head>
-
-    <title> web page</title>
-    </head><body>
-    <h1> Indian country</h1>
-    <h2> Madhya Pradesh</h2>
-    <p> Indore MP </p>
-    <p> Khandwa MP</p>
-    <p> Bhopal mp</p>
-</body>
-</html>
+        <head>
+            <style>
+        /* Background color for web page */
+        body {
+            background-color: lightblue;
+        }
+    </style>
+        <title> web page</title>
+       
+        </head><body>
+        <h1> Indian country</h1>
+        <h2> Madhya Pradesh</h2>
+        <p> Indore MP </p>
+        <p> Khandwa MP</p>   <!-- City of Madhya Pradesh -->
+        <p> Bhopal mp</p> 
+    </body>
+    </html>
