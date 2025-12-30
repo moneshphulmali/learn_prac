4e700c9508ce10a40bf1b319b6544819aadc19fb
Monesh Phulmali <moneshphulmali9575@gmail.com>
2025-12-30 11:54:08 +0530

    delete all headings

--- hello.php
+++ hello.php
@@ -4,11 +4,12 @@
 
     <title> web page</title>
     </head>
-    <body> <h1>Monesh Phulmali</h1>
+    <body> <h1>Monesh Phulmali  Khandwa MP Indian  </h1>
     <h2>Khandwa Madhya Pradesh india</h2>
     <h2>Indore Madhya Pradesh india</h2>
     <h2>Bhopal Madhya Pradesh india</h2>
     <h2>Burhanpure Madhya Pradesh india</h2>
         <h2>Nakpur Madhya Pradesh india</h2>
+        <h2> Ujjain Madhya Pradesh india</h2>
 </body>
 </html>
